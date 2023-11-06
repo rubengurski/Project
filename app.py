2371b116fcb27bcd9ecd061c0ddd193d75da0afb
L3m0n-ade <halima.sb24@gmail.com>
2023-11-06 11:19:01 +0100

    Menu - upload/pay

--- app.py
+++ app.py
@@ -31,28 +31,33 @@ def add_pizza():
     print('Data received:', addpizza, '\n current overview:', Overview)  # See console...
     return redirect('/')
 
-@app.route('/overview', methods=['POST', 'GET'])
-def overview_remove_pay():
+@app.route('/pay', methods=['POST', 'GET'])
+def overview_pay():
     global TotalPrice, scroll
     scroll='home'
     submit=str(request.form['submit'])
     if submit=='Pay & Proceed':
         return redirect('/confirmed')
-    # removePizza=str(request.form['removePizza'])
-    # elif removePizza!='':
-    #     if removePizza=='removeMargherita':
-    #         Overview.remove('Margherita')
-    #         TotalPrice-=5.99
-    #         return redirect('/')
-    #     elif removePizza=='removePepperoni':
-    #         Overview.remove('Pepperoni')
-    #         TotalPrice-=6.99
-    #         return redirect('/')
-    #     elif removePizza=='removeTuna':
-    #         Overview.remove('Tuna')
-    #         TotalPrice-=7.99
-    #         return redirect('/')
+
+@app.route('/removepizza', methods=['POST', 'GET'])
+def overview_remove():
+    global TotalPrice, scroll
+    scroll='home'
+    removePizza=str(request.form['removePizza'])
+    if removePizza=='removeMargherita':
+        Overview.remove('Margherita')
+        TotalPrice-=5.99
+        return redirect('/')
+    elif removePizza=='removePepperoni':
+        Overview.remove('Pepperoni')
+        TotalPrice-=6.99
+        return redirect('/')
+    elif removePizza=='removeTuna':
+        Overview.remove('Tuna')
+        TotalPrice-=7.99
+        return redirect('/')
 
 @app.route('/confirmed', methods=['GET', 'POST'])
 def payment_confirmed():
+
     return render_template('Confirmed.html')
