6eb35828d0821ce92b7af2a5bb77763383964c2f
L3m0n-ade <halima.sb24@gmail.com>
2023-11-07 14:14:53 +0100

    Mario's page works now
    (can't remove items yet, but can add & pay)

--- app.py
+++ app.py
@@ -7,8 +7,10 @@ from datetime import datetime
 app = Flask(__name__)
 
 Overview=[]
+OverviewMario=[]
 Orders=[]
 TotalPrice=0
+TotalPriceMario=0
 scroll=''
 uniqueID=''
 totalOrder = []
@@ -22,7 +24,7 @@ def home():
 
 @app.route('/mario', methods=['POST', 'GET'])
 def mario():
-    return render_template('Mario.html')
+    return render_template('Mario.html', OverviewMario=OverviewMario, Margherita='Margherita', Pepperoni='Pepperoni', Tuna='Tuna', TotalPriceMario=TotalPriceMario)
 
 @app.route('/luigi', methods=['POST', 'GET'])
 def luigi():
@@ -81,6 +83,66 @@ def overview_remove():
         TotalPrice-=7.99
         return redirect('/')
 
+@app.route('/addpizzamario', methods = ['POST'])
+def add_pizza_mario():
+    global TotalPriceMario
+    addpizzamario = str(request.form['addpizzamario'])
+    if addpizzamario not in OverviewMario:
+        if addpizzamario=='Margherita':
+            TotalPriceMario+=5.99
+            OverviewMario.append(addpizzamario)
+        if addpizzamario=='Pepperoni':
+            TotalPriceMario+=6.99
+            OverviewMario.append(addpizzamario)
+        if addpizzamario=='Tuna':
+            TotalPriceMario+=7.99
+            OverviewMario.append(addpizzamario)
+    scroll='home'
+
+    print('Data received:', addpizzamario, '\n current overview Mario:', OverviewMario)  # See console...
+    return redirect('/mario')
+
+@app.route('/removepizzamario', methods=['POST', 'GET'])
+def overview_remove_mario():
+    global TotalPriceMario
+    scroll='home'
+    removepizzamario=str(request.form['removepizzamario'])
+    if removepizzamario=='removeMargherita':
+        OverviewMario.remove('Margherita')
+        TotalPriceMario-=5.99
+        return redirect('/mario')
+    elif removepizzamario=='removePepperoni':
+        OverviewMario.remove('Pepperoni')
+        TotalPriceMario-=6.99
+        return redirect('/mario')
+    elif removepizzamario=='removeTuna':
+        OverviewMario.remove('Tuna')
+        TotalPriceMario-=7.99
+        return redirect('/mario')
+
+@app.route('/paymario', methods=['POST', 'GET'])
+def overview_mario_pay():
+    global uniqueID
+    global totalOrder
+    global allOrders
+    current_time = datetime.now().time()
+    uniqueID = current_time.hour * 3600 + current_time.minute * 60 + current_time.second
+    totalOrder.append(uniqueID)
+    for item in Overview:
+        totalOrder.append(item)
+    print(totalOrder)
+    allOrders.append((totalOrder))
+
+    return redirect('/resetmario')
+
+@app.route('/resetmario', methods=['POST', 'GET'])
+def reset_mario():
+    global OverviewMario
+    global TotalPriceMario
+    OverviewMario = []
+    TotalPriceMario = 0.00
+    return redirect ('/mario')
+
 @app.route('/pay', methods=['POST', 'GET'])
 def overview_pay():
     global uniqueID
@@ -106,6 +168,5 @@ def reset():
 
 @app.route('/confirmed', methods=['GET', 'POST'])
 def payment_confirmed():
-    
     return render_template('Confirmed.html', Overview=Overview, Margherita='Margherita', Pepperoni='Pepperoni', Tuna='Tuna', uniqueID = uniqueID)
 app.run(host='192.168.0.101')
